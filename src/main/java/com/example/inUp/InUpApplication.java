1d858d5c46dc4952235603f12242457aba71cff0
roalwh <nmnmnmlk6248@gmail.com>
2023-10-24 14:48:34 +0900

    gitgnore 수정

---
+++ src/main/java/com/example/inUp/InUpApplication.java
@@ -0,0 +1,13 @@
+package com.example.inUp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/inUp/InUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.inUp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
